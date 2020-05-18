5e49be0db87c7bcee4d43525ceafb40c37102e10
Adam Dawoodjee <adam.daveed@gmail.com>
2020-05-19 00:21:14 +0200

    namecheap api completed, roadmap drawn

--- domian.py
+++ domian.py
@@ -1,60 +1,69 @@
 import requests
-import socket
+import xml.etree.ElementTree as ET
+import json
+
 user="duncannkhata"
 api_key = "c01bb036e0074f26ab3a6bb56f4b31ea"
-client_ip = "41.223.116.249"
-domain = "&SLD=duncan&TLD=com"
+client_ip = "165.56.181.198"
 
 base_url = "https://api.sandbox.namecheap.com/xml.response?ApiUser=" + user + "&ApiKey=" + api_key + "&UserName=" + user + "&ClientIp="+ client_ip
 
-get_command = "&Command=namecheap.domains.dns.getHosts"
-get_url = base_url + get_command + domain
+def get_hosts(domain):
+    sld = domain.split('.')[0]
+    tld = domain.split('.')[1]
 
-get_response = requests.get(get_url)        # To execute get request 
-#print(response, "<br>")     # To print http response code  
-# print(get_response.text)            # To print formatted JSON response
-
-# Name="erp" Type="A" Address="1.1.1.1" MXPref="10" TTL="60"
-
-#New Host
-
-post_command = "&Command=namecheap.domains.dns.setHosts"
-# add old host records to new record here using for loop
-records = "&HostName1=mynewcomp&RecordType1=A&Address1=12.56.67.78&TTL1=60"
-
-post_url = base_url + post_command + domain + records
-
-post_response = requests.post(post_url)
-print(post_response.text)
-
-print("*************************** \n",get_response.text) 
+    domain = "&SLD={sld}&TLD={tld}".format(sld = sld, tld = tld)
+    get_command = "&Command=namecheap.domains.dns.getHosts"
+    get_url = base_url + get_command + domain
+    get_response = requests.get(get_url)        # To execute get request 
+    return get_response
 
 
 """
-get domain records:
-- duncan.com: {"type": A, "host": erp, "value":"1.1.1.1", "TTL":"1M"}
-
-insert new domain records
-- &HostName2=mynewcomp&RecordType2=A&Address2=12.56.67.78&TTL2=1000
-
-
-join old record
-- hosts = []
-i = 1
-
-post_url = base_url
-
-for record in response:
-    host = record.getxml("Name")
-    addre = record.getxml("Address")
-    TTL = record.getxml("TTL")
-    host_type = record.getxml("Type")
-    
-    host_label = "&HostName" + str(i) + "=" + host +  # repeat for other columns...
-    new record = host_label + host_name + host_address + TTL
-
-
-    
-    
+TODO
+- Add error checking, input validation
+- connection testing
+- add namecheap vars to settings like (client_ip - use server ip), username
+- verify successful get, post, stop if not successful
 """
+def add_host(sub_domain, server_ip):
+    sld = sub_domain.split('.')[1]
+    tld = sub_domain.split('.')[2]
 
+    domain = "&SLD={sld}&TLD={tld}".format(sld = sld, tld = tld)
+    existing_hosts = get_hosts(sld+"."+tld)
+    
+    post_command = "&Command=namecheap.domains.dns.setHosts"
+    # add old host records to new record here using for loop
+
+    #print("*************************** \n", existing_hosts.text)
+
+    #Read the existing domain record
+    #my_dict = json.loads(xmltodict.parse(existing_hosts.text), indent=2)
+    root = ET.fromstring(existing_hosts.text)
+    hosts = ""
+    all_hosts = ""
+    i = 1
+    for host in root[3][0]:
+        #if('Domain' in str(child.attrib)): # test whether response has domains
+
+        h = json.loads(json.dumps(host.attrib, indent=2))
+        hosts = "&HostName{0}={name}&RecordType{0}={type}&Address{0}={addr}&TTL{0}={ttl}" \
+                "".format(i, name = h['Name'], type = h['Type'], addr =  h['Address'], ttl =  h['TTL'])
+        all_hosts = all_hosts + hosts
+        i = i+1
+
+    new_host = "&HostName{0}={name}&RecordType{0}={type}&Address{0}={addr}&TTL{0}={ttl}" \
+                "".format(i, name = sub_domain.split('.')[0], type = "A", addr = server_ip, ttl = 700)
+    all_hosts = all_hosts + new_host
+    print(all_hosts)
+    
+    post_url = base_url + post_command + domain + all_hosts
+    
+    post_response = requests.post(post_url)
+    #print(post_response.text)
+    
+
+add_host("fourth.duncan.com", "1.1.1.1")
+
+print(get_hosts("duncan.com").text)
